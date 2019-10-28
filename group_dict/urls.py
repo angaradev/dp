2f0f2ed50251304eca3cf13e1b2b6219b32eb2c7
Vladimir <angara99@gmail.com>
2019-10-28 13:08:39 +0300

    A version of kernel works done

--- group_dict/urls.py
+++ group_dict/urls.py
@@ -1,10 +1,12 @@
 
 from django.contrib import admin
 from django.urls import path
-from .views import insert_kernel, main_work, check_group, change_group
+from .views import insert_kernel, main_work, check_group, change_group, view_groups, view_group
 urlpatterns = [
             path('insertkernel/', insert_kernel, name='insert_kernel'),
             path('mainwork/', main_work, name='main_work'),
             path('checkgroup/', check_group, name='check_group'),
             path('changegroup/<int:pk>/', change_group, name='change_group'),
+            path('viewgroups/', view_groups, name='view_groups'),
+            path('viewgroup/<int:pk>/', view_group, name='view_group'),
         ]
--- group_dict/views.py
+++ group_dict/views.py
@@ -12,7 +12,11 @@ from django.http import JsonResponse
 from django.core import serializers
 import json
 from products.models import Categories
+from django.db.models import Count
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def insert_kernel(request):
     path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'files/kernel2.csv')
     path2 = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'files/parts1.csv')
@@ -26,7 +30,7 @@ def insert_kernel(request):
             }
     return render(request, 'admin/dictionary/insert.html', context)
 
-
+@login_required
 def change_group(request, pk):
     qs = Groups.objects.get(id=pk)
     plus = qs.plus.split('\n')
@@ -50,9 +54,8 @@ def change_group(request, pk):
 
 
 
+@login_required
 def main_work(request):
-
-     
     ker_qs = Kernel.objects.all().exclude(chk=True).order_by('?')[:100]
     nom_qs = Nomenklatura.objects.all().exclude(chk=True).order_by('?')[:100]
     group_qs = Groups.objects.all().order_by('name')
@@ -83,23 +86,33 @@ def main_work(request):
                     defaults={
                             'plus':  '\n'.join(plus),
                             'minus': '\n'.join(minus),
-                            'parent_id' : parent,
+                            'parent_id' : parent.id,
                     }
                     )
             ker_qs.update(chk=True)
             nom_qs.update(chk=True)
             return redirect('dictionary:main_work')
+
+    #Aggregate statistics
+    group_count = Groups.objects.count()
+    ker_count = Kernel.objects.filter(chk=True).count()
+    ker_count_tot = Kernel.objects.count()
+    nom_count = Nomenklatura.objects.filter(chk=True).count()
+    nom_count_tot = Nomenklatura.objects.count()
+    counts = {'gk': group_count, 'kk': ker_count, 'nk': nom_count, 'kk_tot': ker_count_tot, 'nk_tot': nom_count_tot}
+    
     context = {
             'kernels': ker_qs,
             'nomenklatura': nom_qs,
             'groups': group_qs,
             'key_form': key_form,
+            'counts': counts,
             }
     return render(request, 'admin/dictionary/listing.html', context)
 
 
+@login_required
 def check_group(request):
-
     group_name = request.GET.get('group_data')
     chk = Groups.objects.get(name=group_name)
     if chk:
@@ -111,46 +124,35 @@ def check_group(request):
                 }
         return JsonResponse(data)
 
-
-
-
-
-def main_work_back(request):
-     
-    pl = request.GET.get('plus').split('\n')
-    pl = [x.strip() for x in pl]
-    mi = request.GET.get('minus').split('\n')
-    mi = [x.strip() for x in mi]
+@login_required
+def view_group(request, pk):
+    group = Groups.objects.get(id=pk)
+    plus = group.plus.split('\n')
+    plus = [x.strip() for x in plus]
+    minus = group.minus.split('\n')
+    minus = [x.strip() for x in minus]
     ker_qs = Kernel.objects.filter(reduce(operator.or_, (Q(keywords__icontains=x) for x in
-            pl))).exclude(reduce(operator.or_, (Q(keywords__icontains=x) for x in mi))).order_by('keywords')
+        plus))).exclude(reduce(operator.or_, (Q(keywords__icontains=x) for x in minus)))
     nom_qs = Nomenklatura.objects.filter(reduce(operator.or_, (Q(name__icontains=x) for x in
-            pl))).exclude(reduce(operator.or_, (Q(name__icontains=x) for x in mi))).order_by('name')
-    group_qs = Groups.objects.all()
-    key_form = KeyWordForm(request.GET)
-    if key_form.is_valid():
-        plus = key_form.cleaned_data['plus'].split('\n')
-        plus = [x.strip() for x in plus]
-        minus = key_form.cleaned_data['minus'].split('\n')
-        minus = [x.strip() for x in minus]
-
-        ker_qs = ker_qs.filter(reduce(operator.or_, (Q(keywords__icontains=x) for x in
-            plus))).exclude(reduce(operator.or_, (Q(keywords__icontains=x) for x in minus)))
-        nom_qs = nom_qs.filter(reduce(operator.or_, (Q(name__icontains=x) for x in
-            plus))).exclude(reduce(operator.or_, (Q(name__icontains=x) for x in minus)))
-        ker_qs_json = serializers.serialize('json', ker_qs)
-        nom_qs_json = serializers.serialize('json', nom_qs)
-        if request.is_ajax():
-            data = {
-                    'keys': ker_qs_json,
-                    'noms': nom_qs_json,
-                    }
-            return JsonResponse(data, safe=False) 
-        if requset.GET.get('save'):
-            print('save in request')
+        plus))).exclude(reduce(operator.or_, (Q(name__icontains=x) for x in minus)))
     context = {
-            'kernels': ker_qs,
-            'nomenklatura': nom_qs,
-            'groups': group_qs,
-            'key_form': key_form,
+            'group': group,
+            'plus': plus,
+            'minus': minus,
+            'ker': ker_qs,
+            'nom': nom_qs,
             }
-    return render(request, 'admin/dictionary/listing.html', context)
+    return render(request, 'admin/dictionary/group_view.html', context)
+
+@login_required
+def view_groups(request):
+    groups = Groups.objects.all()
+    def chunks(l, n):
+        for i in range(0, len(l), n):
+            yield l[i:i+n]
+    gl = [x for x in chunks(groups, 100)]
+    context = {
+            'groups': gl,
+            'count': groups.count(),
+            }
+    return render(request, 'admin/dictionary/groups_view.html', context)
